1fd04a90a00186f52738ff3d242f6e7fcaff42e4
saifurrehman15 <saifkhandada72@gmail.com>
2024-02-26 17:08:40 +0500

    add index.html and app.js

---
+++ app.js
@@ -0,0 +1,143 @@
+// //Chapter1
+
+
+
+// //Q1
+
+
+// alert("Welcome to my website")
+
+
+// //Q2
+
+// alert("Error! Please Enter a Valid Password")
+
+
+// //Q3
+
+// alert("Welcome to Js Land...\nHappy Coding!")
+
+
+// //Q4
+
+// alert("Welcome to Js Land...")
+
+// //Q5
+
+// console.log("Hello... I can JS through my web browser's console")
+
+
+
+// //Chapter2
+
+
+// //Q1
+
+// var username;
+
+
+// //Q2
+
+// var myName = "Saif Ur Rehman";
+
+
+
+// //Q3
+
+// var message = "Hello World"
+
+// alert(message)
+
+
+
+// //Q4
+
+// var myName ="Saif"
+
+// alert (myName)
+
+// var Age ="19"
+
+// alert (Age)
+
+// var course ="Web & App Development"
+
+// alert (course)
+
+
+// //Q5
+
+// var food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+
+// alert(food)
+
+
+// //Q6
+
+// var EmailAddress = "My Email Address is saifrizwankhan786@gmail.com";
+
+
+// alert(EmailAddress);
+
+
+
+
+// //Q7
+
+
+// var book = "I am trying to learn from the Book A Smarter \nWay to learn Javascript"
+
+
+// alert(book)
+
+
+// //Q8
+
+// document.write("Yah! I can Write HTML content through Javascript")
+
+
+
+// //Q9
+
+// var my = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+
+// alert(my)
+
+
+
+
+
+// //Chapter3
+
+
+// //Q1
+
+// var age = "I am 19 years old"
+
+// alert(age)
+
+
+// //Q2
+
+
+// var visitors = "You have visited this site  10 times"
+
+// alert(visitors)
+
+
+// //Q3
+
+
+// var birthYear = "My birth year is 2005"
+
+// document.write(birthYear)
+
+
+// //Q4
+
+// var information = "John Doe ordered 5 T-shirt(s) on Kids Bazaar."
+
+// document.write(information)
+
+// //Q5
