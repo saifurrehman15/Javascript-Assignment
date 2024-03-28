e1bc3056cb53a1d6eda058c4c7c7f7a7a30774a0
saifurrehman15 <saifkhandada72@gmail.com>
2024-03-29 03:41:03 +0500

    update all

--- chap 1-20.js
+++ chap 1-20.js
@@ -1117,13 +1117,15 @@
 
 // factorial
 // var promptmy = prompt("enter any number");
+// document.write("<h1>factorial of " + promptmy + " " + " is <br> </h1>" )
 
 // for(var i = promptmy - 1 ; i >= 0;--i){
-//     document.write( promptmy * i + "<br>");
+//     document.write(promptmy + " x " + i + " =" + promptmy * i + "<br>");
 // }
 
 
-// var my = prompt("Enter any word which is reversible but not changed");
+
+// // var my = prompt("Enter any word which is reversible but not changed");
 // var upper = my.slice(0,my.length).toUpperCase();
 
 // if(upper === upper.split('').reverse('').join('')){
@@ -1151,6 +1153,209 @@
 //     for(var j=1;j<=i;j++){
 //         document.write("*")
 //     }
-    
+
 //     document.write("<br>")
+// }
+
+
+// for(var i = 1;i<=10;i++){
+//     for(var j=1;j<=10;j++){
+//         document.write(i + "x" + j + "=" + j * i + " <br>   " )
+//     }
+//     document.write("<br>")
+// }
+// //count vowels
+// var prr =prompt("Enter Alphabets");
+
+// for(var i =1;i<=prr.length;i++){
+//     if(prr.length === "a" && prr.length === "e" &&prr.length ===  "i" && prr.length === "o" &&prr.length ===  "u"){
+//         document.write(i)
+//     }
+// }
+
+
+// var cars = ['Mehran','Bmw','Cultus','Audi','Suzuki'];
+
+// var userInp =prompt("Enter your Cars");
+
+// var up22= userInp.slice(0,1).toUpperCase();
+// var up23= userInp.slice(1).toLowerCase();
+// var joi = up22 + up23;
+// var inn = cars.indexOf(joi);
+
+
+// for(var i = 0; i<=cars.length ; i++){
+// if(joi === cars[i]){
+//     document.write(cars[i] + "Found on" + " " + inn);
+//     break
+// }
+// }
+
+// if(joi !== cars[i]){
+//     alert("Car not found")
+// }
+
+
+
+
+// Chapter 17-29
+
+
+//Q1
+
+// var arr = [[],[],[],[]];
+
+
+
+//Q2
+
+
+// var arr2 = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
+
+// for (var i = 0; i < arr2.length; i++) {
+//         document.write(arr2[i][j] + " ");
+//     document.write("<br>")
+// }
+
+
+//Q3
+
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(i + "<br>")
+// }
+
+
+//Q4
+
+// var table = prompt("Enter any table number");
+// var tablelen = prompt("Enter table length");
+
+// for(var i =1; i <= tablelen;i++){
+// document.write(table + "x" + i + "=" + table * i + "<br>")
+// }
+
+
+//Q5
+
+// var fruits = ['apple', 'banana', 'mango', 'orange','strawberry'];
+
+// for(var i= 0;i< fruits.length;i++){
+//     document.write(fruits[i] + "<br>")
+//     for(var j =i ;j <= i;j++){
+//     document.write("Element at " + i + " index is " + fruits[i] + "<br>")
+// }
+// }
+
+
+//Q6
+
+
+// document.write("<h3>Counting</h3>");
+
+
+// for(var i = 1; i<= 15;i++){
+//     document.write(i + ",")
+// }
+
+// document.write("<h3>Reverse Counting</h3>");
+
+
+// for(var i = 15; i>= 1;i--){
+//     document.write(i + ",")
+// }
+
+
+// document.write("<h3>Even</h3>");
+
+
+// for(var i = 0; i<= 20;i=i+2){
+//     document.write(i + ",")
+// }
+
+// document.write("<h3>Odd</h3>");
+
+
+// for(var i = 1; i< 20;i=i+2){
+//     document.write(i + ",")
+// }
+
+// document.write("<h3>Series</h3>");
+
+
+// for(var i = 2; i<= 20;i=i+2){
+//     document.write(i + "k" + ",")
+// }
+
+
+
+//Q7
+
+
+// var items = ['Cake', 'Apple pie', 'Cookie', 'Chips', 'Patties'];
+// var bakery = prompt("Welcome to XYZ Bakery.What do you want to order Sir/Madam?");
+// var upp = bakery.slice(0,1).toUpperCase();
+// var low = bakery.slice(1).toLowerCase();
+// var join = upp + low;
+
+// match =false;
+// for(var i =0 ;i<items.length;i++){
+//     if(join === items[i]){
+//         match = true;
+//         alert(`Cookie is available a index ${i} in our bakery`)
+// break
+//     }
+
+// }
+// if(!match){
+//     alert('We are sorry,This item is not available in our bakery')
+// }
+
+
+//Q8
+
+
+// var larnum = [24, 53, 78, 91, 12];
+// var largest = larnum[0];
+
+// document.write("Array items:" + larnum + "<br><br>")
+
+// for (var i = 0; i < larnum.length; i++) {
+//     if (larnum[i] > largest) {
+//         largest = larnum[i];
+//     }
+// }
+// document.write("The largest number is" + largest + "<br>");
+
+
+
+//Q9
+
+
+// var larnum = [24, 53, 78, 91, 12];
+// var smallest = larnum[0];
+
+// document.write("Array items:" + larnum + "<br><br>")
+
+// for (var i = 0; i < larnum.length; i++) {
+//     if (larnum[i] < smallest) {
+//         smallest = larnum[i];
+//     }
+// }
+// document.write("The Smallest number is" + smallest + "<br>");
+
+
+//Q10
+
+//Method1
+
+// for(var i =1;i<=20;i++){
+//     document.write(5 * i + ",")
+// }
+
+
+//Method2
+
+// for(var i =5;i<=100;i=i+5){
+//     document.write(i + ",")
 // }
